9bc60ff9d8d564dc305c0e0c8ff9f291977b6bbe
Keiho Sakapon <git@saka-pon.net>
2018-04-02 16:46:54 +0900

    Require HTTPS connection

--- RandomData2/RandomDataWebApi/Global.asax.cs
+++ RandomData2/RandomDataWebApi/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -14,6 +16,32 @@ namespace RandomDataWebApi
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (!Request.IsSecureConnection &&
+                RequireHttps &&
+                !string.Equals(Request.Url.Host, "localhost", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // If we use Uri.OriginalString property, ":80" may be appended.
+                var secureUrl = Regex.Replace(Request.Url.AbsoluteUri, @"^\w+(?=://)", Uri.UriSchemeHttps);
+
+                if (PermanentHttps)
+                    Response.RedirectPermanent(secureUrl, true);
+                else
+                    Response.Redirect(secureUrl, true);
+            }
+        }
+
+        static bool RequireHttps
+        {
+            get { return Convert.ToBoolean(ConfigurationManager.AppSettings["app:RequireHttps"]); }
+        }
+
+        static bool PermanentHttps
+        {
+            get { return Convert.ToBoolean(ConfigurationManager.AppSettings["app:PermanentHttps"]); }
         }
     }
 }
