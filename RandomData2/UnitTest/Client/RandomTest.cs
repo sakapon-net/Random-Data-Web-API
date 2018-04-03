0eaf95049ae4edd9b1f3683e47c9bc861dfbd754
Keiho Sakapon <git@saka-pon.net>
2018-04-03 11:39:12 +0900

    Add test

--- RandomData2/UnitTest/Client/RandomTest.cs
+++ RandomData2/UnitTest/Client/RandomTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using Blaze.Randomization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,6 +10,49 @@ namespace UnitTest.Client
     [TestClass]
     public class RandomTest
     {
+        static readonly Dictionary<int, HttpStatusCode> StatusCodes_4096 = new Dictionary<int, HttpStatusCode>
+        {
+            { -1, HttpStatusCode.NotFound },
+            { 0, HttpStatusCode.OK },
+            { 4096, HttpStatusCode.OK },
+            { 4097, HttpStatusCode.NotFound },
+        };
+
+        [TestMethod]
+        async public Task NewAlphabets_Bound()
+        {
+            foreach (var item in StatusCodes_4096)
+                Assert.AreEqual(item.Value, await HttpHelper.GetAsync_StatusCode($"api/NewAlphabets/{item.Key}"));
+        }
+
+        [TestMethod]
+        async public Task NewAlphanumerics_Bound()
+        {
+            foreach (var item in StatusCodes_4096)
+                Assert.AreEqual(item.Value, await HttpHelper.GetAsync_StatusCode($"api/NewAlphanumerics/{item.Key}"));
+        }
+
+        [TestMethod]
+        async public Task NewBytes_HexLower_Bound()
+        {
+            foreach (var item in StatusCodes_4096)
+                Assert.AreEqual(item.Value, await HttpHelper.GetAsync_StatusCode($"api/NewBytes/hexlower/{item.Key}"));
+        }
+
+        [TestMethod]
+        async public Task NewBytes_HexUpper_Bound()
+        {
+            foreach (var item in StatusCodes_4096)
+                Assert.AreEqual(item.Value, await HttpHelper.GetAsync_StatusCode($"api/NewBytes/hexupper/{item.Key}"));
+        }
+
+        [TestMethod]
+        async public Task NewBytes_Base64_Bound()
+        {
+            foreach (var item in StatusCodes_4096)
+                Assert.AreEqual(item.Value, await HttpHelper.GetAsync_StatusCode($"api/NewBytes/base64/{item.Key}"));
+        }
+
         [TestMethod]
         async public Task NewUuid()
         {
