5a95eabbcad5773e2d69fdf8162516c94f127e2c
Keiho Sakapon <git@saka-pon.net>
2018-04-03 11:30:05 +0900

    Add helper method

--- RandomData2/UnitTest/Client/HttpHelper.cs
+++ RandomData2/UnitTest/Client/HttpHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -23,6 +24,15 @@ namespace UnitTest.Client
 
         public static Task<T> GetAsync<T>(string uri, object query) =>
             GetAsync<T>(AddQuery(uri, query));
+
+        async public static Task<HttpStatusCode> GetAsync_StatusCode(string uri)
+        {
+            using (var http = new HttpClient { BaseAddress = BaseUri })
+            {
+                var response = await http.GetAsync(uri);
+                return response.StatusCode;
+            }
+        }
 
         public static string AddQuery(string uri, object query) => $"{uri}?{query.ToFormUrlEncoded()}";
 
