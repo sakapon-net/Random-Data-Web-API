669df787853e26466dfd0ea22d0ca95926644fcf
Keiho Sakapon <git@saka-pon.net>
2018-04-06 17:58:19 +0900

    Set sample objects for type

--- RandomData2/RandomDataWebApi/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ RandomData2/RandomDataWebApi/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -36,14 +36,15 @@ namespace RandomDataWebApi.Areas.HelpPage
             // Uncomment the following to use the documentation from XML documentation file.
             config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/bin/RandomDataWebApi.xml")));
 
-            //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
-            //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
-            //// formats by the available formatters.
-            //config.SetSampleObjects(new Dictionary<Type, object>
-            //{
+            // Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
+            // Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
+            // formats by the available formatters.
+            config.SetSampleObjects(new Dictionary<Type, object>
+            {
+                { typeof(int), 8 },
             //    {typeof(string), "sample string"},
             //    {typeof(IEnumerable<string>), new string[]{"sample 1", "sample 2"}}
-            //});
+            });
 
             // Extend the following to provide factories for types not handled automatically (those lacking parameterless
             // constructors) or for which you prefer to use non-default property values. Line below provides a fallback
